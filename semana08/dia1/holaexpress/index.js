5b5f229ac4c6d07758b8a35f6ec6a81b7f7406e6
WillyRLV <williamlv1998@gmail.com>
2022-05-31 18:10:40 -0500

    empezando semana 8 dia 1

---
+++ semana08/dia1/holaexpress/index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+
+const app = express()
+const port = 5000;
+
+app.get('/',(req,res)=>{
+    res.send('<center><h1>BIENVENIDO AMI SITIO WEB CON EXPRESS</h1></center>')
+})
+
+app.get('/hola/:nombre',(req,res)=>{
+    res.send('<center><h1>Hola '+ req.params.nombre + '</h1></center>');
+})
+
+app.get('/saludojson',(req,res)=>{
+    res.json(
+        {
+            'saludo':'hola'
+        }
+    );
+})
+
+app.listen(port,function(){
+    console.log("servidor activo en http://localhost:"+port)
+})
